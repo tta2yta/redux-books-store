b717ce74658f705fe9f3e1059674392eb7fd4d1a
tta2yta <tta2yta@gmail.com>
2021-08-30 15:01:46 +0300

    Add books array objects:

--- src/components/book/BookList.js
+++ src/components/book/BookList.js
@@ -1,5 +1,25 @@
 
 const bookList=()=>{
+    const books=[
+        {
+            id: 1,
+            title:"Book-1",
+            author:"AAA",
+            categories:"category -1"
+        },
+        {
+            id: 2,
+            title:"Book-2",
+            author:"BBB",
+            categories:"category -2"
+        }
+        {
+            id: 1,
+            title:"Book-3",
+            author:"CCC",
+            categories:"category -3"
+        }
+    ]
 
 }
 
