cfbaeaab4dd28e9af12e78a8ab2907261e8f5efd
tta2yta <tta2yta@gmail.com>
2021-08-31 12:58:36 +0300

    Add getBooksApi action

--- src/redux/book/action/bookAction.js
+++ src/redux/book/action/bookAction.js
@@ -1,5 +1,10 @@
+// import axios from 'axios';
+
 const ADD_BOOK = 'ADD_BOOK';
 const REMOVE_BOOK = 'REMOVE_BOOK';
+const FETCH_BOOKS_API = 'FETCH_BOOKS_API';
+
+const apiUrl = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/cF90aS533hJGfCJadIOC/books';
 
 export const addBook = (payLoad) => ({
   type: ADD_BOOK,
@@ -10,3 +15,34 @@ export const removeBook = (payLoad) => ({
   type: REMOVE_BOOK,
   payLoad,
 });
+
+export const getBooksApi = () => async (dispatch) => {
+  const books = [];
+  const response = fetch(apiUrl,
+    {
+      method: 'GET',
+      headers: { 'content-type': 'application/json' },
+      mode: 'cors',
+    }).then((res) => res.json())
+    .then((err) => err);
+  if (response) {
+    // eslint-disable-next-line no-restricted-syntax
+    for (const [key, value] of Object.entries(response)) {
+      const id = key;
+      const obj = value;
+      const book = {
+        item_id: id,
+        title: obj[0].title,
+        category: obj[0].category,
+      };
+      books.push(book);
+    }
+
+    dispatch({
+      type: FETCH_BOOKS_API,
+      payLoad: books,
+    });
+  } else {
+    console.log('Unable to fetch data');
+  }
+};
