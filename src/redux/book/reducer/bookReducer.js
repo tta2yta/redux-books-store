b1369dc3217e06802551f9470d9a13afd9e58753
tta2yta <tta2yta@gmail.com>
2021-08-31 12:46:13 +0300

    Add case fetch_books_api

--- src/redux/book/reducer/bookReducer.js
+++ src/redux/book/reducer/bookReducer.js
@@ -6,6 +6,8 @@ const bookReducer = (state = initialState, action) => {
       return [...state, action.payLoad];
     case 'REMOVE_BOOK':
       return state.filter((item) => item.item_id !== action.payLoad);
+    case 'FETCH_BOOKS_API':
+    return {...state, books : action.payLoad}
     default:
       return state;
   }
