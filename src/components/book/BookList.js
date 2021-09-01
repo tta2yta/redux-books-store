bf2687bf9dcbccffbb9f9d96a7bca460c51f8557
tta2yta <tta2yta@gmail.com>
2021-09-01 12:07:21 +0300

    Add styles to progress

--- src/components/book/BookList.js
+++ src/components/book/BookList.js
@@ -1,10 +1,11 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getBooks } from '../../redux/book/action/bookAction';
 import AddRemove from './addRemove';
+import Progress from './Progress';
 
 const bookList = () => {
-  const [books, setBooks] = useState([]);
+  // const [books, setBooks] = useState([]);
   const bookLists = useSelector((state) => state.books);
 
   const dispatch = useDispatch();
@@ -13,32 +14,30 @@ const bookList = () => {
     dispatch(getBooks());
   }, []);
 
-  useEffect(() => {
-    if (books !== bookLists) setBooks(bookLists);
-  });
+  // useEffect(() => {
+  //   if (books !== bookLists) setBooks(bookLists);
+  // });
 
   return (
     <>
       <div className="bookList">
-        {books.map((item) => (
-          <div key={item.item_id} className="list-item">
-            <div className="book-category">{item.category}</div>
-            <div className="book-title">{item.title}</div>
-            <div className="book-author">{item.author}</div>
-            <AddRemove item_id={item.item_id} />
-          </div>
+        {bookLists.map((item) => (
+          <>
+            <div key={item.item_id} className="list-item">
+              <div className="book-category">{item.category}</div>
+              <div className="book-title">{item.title}</div>
+              <div className="book-author">{item.author}</div>
+              <AddRemove item_id={item.item_id} />
+            </div>
+
+            <div className="perc-completed">
+              <Progress value="90" />
+            </div>
+            <div className="chapter">chapter</div>
+          </>
 
         ))}
-        <div className="perc-completed">
-          <div className="c100 p90 blue">
-            <span>90%</span>
-            <div className="slice">
-              <div className="bar" />
-              <div className="fill" />
-            </div>
-          </div>
-        </div>
-        <div className="chapter">chapter</div>
+
       </div>
     </>
   );
--- src/components/book/Progress.js
+++ src/components/book/Progress.js
@@ -1,12 +1,11 @@
 import { CircularProgress } from '@material-ui/core';
-import './Progress.css';
 
 import PropTypes from 'prop-types';
 
 const Progress = ({ value }) => (
   <div className="progress d-flex flex-basis">
     <CircularProgress variant="determinate" size="4.2rem" thickness="2.5" value={value} />
-    <div>
+    <div className="progress-text">
       <span className="percentage">{`${value}%`}</span>
       <span className="completed">Completed</span>
     </div>
